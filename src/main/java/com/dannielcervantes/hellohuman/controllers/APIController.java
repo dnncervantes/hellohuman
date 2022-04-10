da980405308664686bdcc6a438cb77185df07407
Danniel C <dnncervantes.yahoo.com>
2022-04-10 03:27:07 -0700

    first commit

---
+++ src/main/java/com/dannielcervantes/hellohuman/controllers/APIController.java
@@ -0,0 +1,18 @@
+package com.dannielcervantes.hellohuman.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class APIController {
+	@RequestMapping("/")
+	public String index(@RequestParam(value="name", required=false) String searchQuery) {
+		if(searchQuery == null) {
+		return "Hello Human";
+		}
+		else {
+			return "Hello " + searchQuery;
+		}
+	}
+}
